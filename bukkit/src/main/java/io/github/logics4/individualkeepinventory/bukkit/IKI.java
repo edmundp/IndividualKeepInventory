dd2b498233d8c412da699f4a7cad77e39ba2f54c
Edmund Phung <edmund@live.ca>
2023-06-04 16:05:38 -0400

    Changed to drop XP and player heads on death for custom server

--- bukkit/src/main/java/io/github/logics4/individualkeepinventory/bukkit/IKI.java
+++ bukkit/src/main/java/io/github/logics4/individualkeepinventory/bukkit/IKI.java
@@ -24,10 +24,16 @@ import io.github.logics4.individualkeepinventory.common.Constants;
 
 import org.bstats.bukkit.MetricsLite;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IKI extends JavaPlugin implements Listener {
 
@@ -42,10 +48,31 @@ public class IKI extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (event.getEntity().hasPermission(Constants.IKI_KEEPINVENTORY_PERMISSION)) {
+            // Inventory is kept, but XP will be still be lost
             event.setKeepInventory(true);
-            event.setKeepLevel(true);
+
+            // Remove all drops except for player heads
+            // This also handles scenarios where the inventory has player heads
+            ItemStack playerHeadStack = null;
+            for (ItemStack item : event.getDrops()) {
+                if (item.getType() == Material.SKULL_ITEM) {
+                    SkullMeta skull = (SkullMeta)item.getItemMeta();
+                    // Make sure the player head is of the killed player
+                    if (skull.hasOwner() && skull.getOwningPlayer().equals(event.getEntity())) {
+                        // Ensure amount is not more than 1
+                        item.setAmount(1);
+
+                        if (playerHeadStack == null) {
+                            playerHeadStack = item;
+                        }
+                    }
+                }
+            }
+
             event.getDrops().clear();
-            event.setDroppedExp(0);
+            if (playerHeadStack != null) {
+                event.getDrops().add(playerHeadStack);
+            }
         }
     }
 }
